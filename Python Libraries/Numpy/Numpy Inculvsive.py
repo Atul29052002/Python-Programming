aa9cd47c8b60783705135df7bde93c9e3ac1a06b
Atul Khuntia <142527681+Atul29052002@users.noreply.github.com>
2025-06-07 19:32:47 +0530

    Create Numpy Inculvsive.py

---
+++ Python Libraries/Numpy/Numpy Inculvsive.py
@@ -0,0 +1,7 @@
+# Problem statement Create an array with values ranging consecutively from 9 to 49 (both inclusive). Print the Numpy array.
+# Output Format : element1 element2 element3 ..
+
+import numpy as np
+
+a = np.arange(9,50)
+print(*a)
