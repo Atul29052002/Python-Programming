cb655f511dedb0e21bd96eb87730ec460dabc454
Atul Khuntia <142527681+Atul29052002@users.noreply.github.com>
2025-06-07 19:37:19 +0530

    Create Numpy Identity.py

---
+++ Python Libraries/Numpy/Numpy Identity.py
@@ -0,0 +1,9 @@
+# Problem statement Create a matrix having diagonal elements as 1 and all other elements as 0 of size (5, 5). Print the Numpy array.
+# Output Format : numpyArray
+
+import numpy as np
+
+a = np.identity(5,dtype=int)
+print(a)
+
+
