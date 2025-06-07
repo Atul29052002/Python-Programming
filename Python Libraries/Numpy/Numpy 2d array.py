096ec0dfd2cef432961bd4e9aecd8b7f6fd756ec
Atul Khuntia <142527681+Atul29052002@users.noreply.github.com>
2025-06-07 12:57:21 +0530

    Create Numpy 2d array.py

---
+++ Python Libraries/Numpy/Numpy 2d array.py
@@ -0,0 +1,13 @@
+# Problem statement
+# Given a 2D list, create a numpy 2D array using it. Note: Given 2D list is [[1, 2, 3], [4, 5, 6], [7, 8, 9]] Print the Numpy array.
+# Output Format : numpyArray
+
+import numpy as np
+
+a = [[1,2,3],[4,5,6],[7,8,9]]
+b = np.array(a)
+print(b)
+
+
+
+  
